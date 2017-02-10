1271d7156f0c3a6a4b00a59c75b49d93f896f3ec
gogoup <15800321565@163.com>
2017-02-10 16:15:14 +0800

    第三级用户菜单修正

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -81,8 +81,16 @@ class CommonController extends Controller
         if($role>2){
             $projects=D('Project');
             $where['u_id']=$_SESSION['userinfo']['u_id'];
-            $allProject=$projects->allProject($where);
-            $this->assign('project',$allProject);
+          $allPID=$projects->allProjectId($where);
+            $allProject=array();
+            foreach($allPID as $key=>$val)
+            {
+                $allP['p_id']=$val['p_id'];
+                $project=$projects->oneProject($allP);
+                $allProject[]=$project;
+            }
+                  $this->assign('project',$allProject);
+
         }
 
     }
--- Application/Home/Model/ProjectModel.class.php
+++ Application/Home/Model/ProjectModel.class.php
@@ -16,17 +16,27 @@ class  ProjectModel extends Model
     /**
      * @param $where
      * @return mixed    二维数组
-     * 返回符合条件的所有项目
+     * 返回符合条件的所有项目ID
      */
-    public function allProject($where)
+    public function allProjectId($where)
     {
         return $this->UserProject->where($where)->select();
     }
 
     /**
      * @param $where
+     * @return mixed   二维数组
+     * 返回符合条件的所有项目信息
+     */
+    public function allProject($where)
+    {
+        return $this->Project->where($where)->select();
+    }
+
+    /**
+     * @param $where
      * @return mixed  一维数组
-     * 返回符合条件的项目信息
+     * 返回符合条件的一条项目信息
      */
     public function oneProject($where)
     {
